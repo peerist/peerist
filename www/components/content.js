0a3257abd5a0f4514848128fea2669fb1314bd23
Graham Barber <grahammbarber@gmail.com>
2019-04-22 21:57:04 -0700

    feat(snippet): add preview component

---
+++ www/components/content.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled'
+
+const ArticleSnippet = ({ title, preview }) => {
+  return (
+    <div
+      css={{
+        marginBottom: '1.5em',
+        padding: '2em',
+        borderRadius: '7px',
+        boxShadow: '0px 0px 21px rgba(0, 0, 0, 0.05)'
+      }}
+    >
+      <h3 css={{ marginBottom: '1em' }}>{title}</h3>
+      <p css={{ paddingLeft: 0 }}>{preview}</p>
+    </div>
+  )
+}
+
+export { ArticleSnippet }
