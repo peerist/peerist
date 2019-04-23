2dfbfbbd35fbd5b612a65af8e0ce88eae9928125
Graham Barber <grahammbarber@gmail.com>
2019-04-22 21:58:00 -0700

    feat: render demo data

--- www/pages/index.js
+++ www/pages/index.js
@@ -5,7 +5,9 @@ import { animated, useTransition } from 'react-spring'
 
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import { ArticleSnippet } from '../components/content'
 import { useInterval } from '../utils/hooks'
+import Tab from '../components/tabs'
 
 const phraseChanger = function*(phrases) {
   let index = phrases.length
@@ -31,6 +33,7 @@ const phraser = phraseChanger([
 
 const Page = () => {
   const [text, setText] = useState('Science.')
+  const [activeTab, setActiveTab] = useState('Most Recent')
   const transitions = useTransition(text, null, {
     from: { opacity: 0, y: -20 },
     enter: { opacity: 1, y: 0 },
@@ -67,16 +70,49 @@ const Page = () => {
         </Layout>
       </Hero>
       <Layout>
-        <div
+        <main
           css={{
             display: 'flex',
             flexDirection: 'row',
             flexGrow: 1
           }}
         >
-          <div css={{ width: '100%' }}>posts</div>
-          <div css={{ width: '300px' }}>links</div>
-        </div>
+          <div css={{ width: '100%', paddingTop: '2em', paddingRight: '1em' }}>
+            <div css={{ display: 'flex', flexDirection: 'row' }}>
+              <Tab
+                active={activeTab === 'Most Recent'}
+                onClick={() => setActiveTab('Most Recent')}
+              >
+                Most Recent
+              </Tab>
+              <Tab
+                active={activeTab === 'Popular'}
+                onClick={() => setActiveTab('Popular')}
+              >
+                Popular
+              </Tab>
+              <Tab
+                active={activeTab === 'Featured'}
+                onClick={() => setActiveTab('Featured')}
+              >
+                Featured
+              </Tab>
+            </div>
+            <ArticleSnippet
+              title="Ancestral sequences from an elite neutralizer proximal to the development of neutralization resistance as a potential source of HIV vaccine immunogens"
+              preview="Mesa and colleagues identify sequences that may define the transition from neutralization sensitive to resistant envs."
+            />
+            <ArticleSnippet
+              title="Evaluation of intuitive trunk and non-intuitive leg sEMG control interfaces as command input for a 2-D Fitts’s law style task"
+              preview="Verros and co-workers report a non-intuitive control interface for a trunk orthotic device for subjects with Duchenne muscular dystrophy that can be easily used to perform daily tasks."
+            />
+            <ArticleSnippet
+              title="Non-verbal speech cues as objective measures for negative symptoms in patients with schizophrenia"
+              preview="Tahir and colleagues use a machine learning based system to identify non-verbal speech cues that can be used as indices of the negative symptoms of schizophrenia."
+            />
+          </div>
+          <div css={{ width: '400px' }}>links</div>
+        </main>
       </Layout>
     </>
   )
