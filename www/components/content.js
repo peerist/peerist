590b29484339249584b34c9091afcaefc7ed1d60
Graham Barber <grahammbarber@gmail.com>
2019-05-04 12:20:28 -0700

    refactor(content): reduce card title size

--- www/components/content.js
+++ www/components/content.js
@@ -4,13 +4,13 @@ const ArticleSnippet = ({ title, preview }) => {
   return (
     <div
       css={{
-        marginBottom: '1.5em',
         padding: '2em',
+        marginBottom: '1.5em',
         borderRadius: '7px',
         boxShadow: '0px 0px 21px rgba(0, 0, 0, 0.1)'
       }}
     >
-      <h3 css={{ marginBottom: '1em' }}>{title}</h3>
+      <h2 css={{ marginBottom: '1em' }}>{title}</h2>
       <p
         css={{
           fontWeight: 300,
